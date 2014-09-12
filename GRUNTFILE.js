fe99bc368b099c4d6cc764cfbe3517d3948f9b04
Damian Taggart <damian@mindsharestudios.com>
2014-09-12 10:19:25 -0600

    added grunt support files

---
+++ GRUNTFILE.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		less: {
+			development: {
+				files:   {
+					"style.css": "less/style.less"
+				},
+				options: {
+					sourceMap:         true,
+					compress:          true,
+					cleancss:          true,
+					sourceMapFilename: "style.css.map"
+				}
+			}
+		},
+
+		watch: {
+			configFiles: {
+				files:   ['GRUNTFILE.js'],
+				//tasks: ['newer:jshint:all'],
+				options: {
+					reload: true
+				}
+			},
+
+			less: {
+				files: ['less/*.less'],
+				tasks: ['less']
+			}
+
+		}
+	});
+
+	grunt.registerTask('default', ['watch']);
+};
