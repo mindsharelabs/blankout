6aee0ee75864becd29a42d71fa08b874f8d31ffd
Damian Taggart <damian@mindsharestudios.com>
2016-05-10 15:40:27 -0600

    Add equalheight DIV Js function

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Built using:
 ## Changelog
 
 ### v3.8.2
+* 
+* Add equalheight DIV Js function
 * Update title tag output for new WP 4.1+ method
 * Remove old mobile friendly meta tags
 
--- js/main.js
+++ js/main.js
@@ -163,3 +163,50 @@ jQuery(document).ready(function($) {
     $(document.body).show();
 
 });
+
+/**
+ * Function for creating equal height DIVs. Works on page resize for responsiveness.
+ *
+ * @param container
+ */
+equalheight = function(container) {
+
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = [],
+        $el,
+        topPosition = 0;
+    $(container).each(function() {
+
+        $el = $(this);
+        $($el).height('auto')
+        topPostion = $el.position().top;
+
+        if (currentRowStart != topPostion) {
+            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+                rowDivs[ currentDiv ].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+            rowDivs[ currentDiv ].height(currentTallest);
+        }
+    });
+};
+/*
+// usage examples: 
+
+$(window).load(function() {
+    equalheight('.main article');
+});
+
+$(window).resize(function() {
+    equalheight('.main article');
+});
+*/
